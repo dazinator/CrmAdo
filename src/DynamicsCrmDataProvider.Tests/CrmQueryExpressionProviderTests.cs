826526742cd9202a2882f9396e6392ad19158a48
Darrell <darrell.tunnell@googlemail.com>
2014-02-06 16:25:06 +0000

    removed 2 redundant tests

--- src/DynamicsCrmDataProvider.Tests/CrmQueryExpressionProviderTests.cs
+++ src/DynamicsCrmDataProvider.Tests/CrmQueryExpressionProviderTests.cs
@@ -95,63 +95,7 @@ namespace DynamicsCrmDataProvider.Tests
             var queryExpression = subject.CreateQueryExpression(cmd as SelectBuilder);
 
         }
-
-        [Test]
-        public void Where_Clause_Equals_Numeric_Literal_Value()
-        {
-            // Arrange
-            var sql = "Select contactid, title, lastname From contact Where title = 1000000 and lastname = 'Caeser'";
-            var commandBuilder = new CommandBuilder();
-            var cmd = commandBuilder.GetCommand(sql);
-
-            var subject = CreateTestSubject();
-            // Act
-            var queryExpression = subject.CreateQueryExpression(cmd as SelectBuilder);
-            // Assert
-            Assert.That(queryExpression.ColumnSet.AllColumns == false);
-            Assert.That(queryExpression.ColumnSet.Columns[0] == "contactid");
-            Assert.That(queryExpression.ColumnSet.Columns[1] == "title");
-            Assert.That(queryExpression.ColumnSet.Columns[2] == "lastname");
-            Assert.That(queryExpression.EntityName == "contact");
-            Assert.That(queryExpression.Criteria.Conditions.Count, Is.EqualTo(2));
-            Assert.That(queryExpression.Criteria.FilterOperator, Is.EqualTo(LogicalOperator.And));
-            Assert.That(queryExpression.Criteria.Conditions[0].AttributeName == "title");
-            Assert.That(queryExpression.Criteria.Conditions[0].Operator, Is.EqualTo(ConditionOperator.Equal));
-            Assert.That(queryExpression.Criteria.Conditions[0].Values, Contains.Item(1000000));
-
-            Assert.That(queryExpression.Criteria.Conditions[1].AttributeName == "lastname");
-            Assert.That(queryExpression.Criteria.Conditions[1].Operator, Is.EqualTo(ConditionOperator.Equal));
-            Assert.That(queryExpression.Criteria.Conditions[1].Values, Contains.Item("Caeser"));
-
-        }
-
-        [Test]
-        public void Where_Clause_Not_Equals_Numeric_Literal_Value()
-        {
-            // Arrange
-            var sql = "Select contactid, firstname, iscustomer From contact Where firstname <> 'Julius' and iscustomer <> 1";
-            var commandBuilder = new CommandBuilder();
-            var cmd = commandBuilder.GetCommand(sql);
-
-            var subject = CreateTestSubject();
-            // Act
-            var queryExpression = subject.CreateQueryExpression(cmd as SelectBuilder);
-            // Assert
-            Assert.That(queryExpression.ColumnSet.AllColumns == false);
-            Assert.That(queryExpression.ColumnSet.Columns[0] == "contactid");
-            Assert.That(queryExpression.ColumnSet.Columns[1] == "firstname");
-            Assert.That(queryExpression.ColumnSet.Columns[2] == "iscustomer");
-            Assert.That(queryExpression.EntityName == "contact");
-            Assert.That(queryExpression.Criteria.Conditions.Count, Is.EqualTo(2));
-            Assert.That(queryExpression.Criteria.FilterOperator, Is.EqualTo(LogicalOperator.And));
-            Assert.That(queryExpression.Criteria.Conditions[0].AttributeName == "firstname");
-            Assert.That(queryExpression.Criteria.Conditions[0].Operator, Is.EqualTo(ConditionOperator.NotEqual));
-            Assert.That(queryExpression.Criteria.Conditions[0].Values, Contains.Item("Julius"));
-            Assert.That(queryExpression.Criteria.Conditions[1].AttributeName == "iscustomer");
-            Assert.That(queryExpression.Criteria.Conditions[1].Operator, Is.EqualTo(ConditionOperator.NotEqual));
-            Assert.That(queryExpression.Criteria.Conditions[1].Values, Contains.Item(1));
-
-        }
+       
 
         [Test]
         [TestCase("=", "SomeValue", "{0} {1} '{2}'", TestName = "Equals Filter with a String Constant")]
