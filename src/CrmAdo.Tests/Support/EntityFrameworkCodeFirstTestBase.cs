dd5c8860818444bfb27a1af9023d10c50ae2bcf6
darrell <darrell.tunnell@googlemail.com>
2014-08-18 09:17:43 +0100

    added basic ef test with blog context

---
+++ src/CrmAdo.Tests/Support/EntityFrameworkCodeFirstTestBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrmAdo.Tests.Support
+{
+    public class EntityFrameworkCodeFirstTestBase<T> : BaseTest<T> where T : DbContext
+    {
+        public EntityFrameworkCodeFirstTestBase(string connectionString)
+        {
+            this.ConnectionString = connectionString;
+        }
+
+        public string ConnectionString { get; set; }
+
+        protected override T CreateTestSubject()
+        {
+            return base.CreateTestSubject(ConnectionString);
+        }
+    }
+}
