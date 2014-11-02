6556ec46e49ee543a3447bd8b2a9e69b12e92e14
dazinator <darrell.tunnell@googlemail.com>
2014-11-02 23:11:55 +0000

    #33

--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -5,5 +5,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © Darrell Tunnell 2014")]
 
 //DO NOT MODIFY - THESE ARE SET BY TEAMCITY
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+//[assembly: AssemblyVersion("1.0.0.0")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
