a61b3c3ebe695ac7d3c503b5a161e774f1b3fa86
dazinator <darrell.tunnell@googlemail.com>
2015-05-31 22:51:14 +0100

    #67 added test coverage for selecting columns using schema and tablename prefix identifiers.

--- src/CrmAdo.Tests/Tests/SelectTests/SelectStatementProjectionVisitorTests.cs
+++ src/CrmAdo.Tests/Tests/SelectTests/SelectStatementProjectionVisitorTests.cs
@@ -66,5 +66,25 @@ namespace CrmAdo.Tests
             }
         }
 
+        [Category("Projecting")]
+        [Test(Description = "Should support Column Names with Dbo prefix")]
+        public void Should_Support_Select_Statement_From_Dbo_Prefixed_Column_Names()
+        {
+            // Arrange
+            var sql = "Select dbo.contact.contactid, dbo.contact.firstname, dbo.contact.lastname From dbo.contact";
+            using (var sandbox = RequestProviderTestsSandbox.Create())
+            {
+
+                // Act
+                var queryExpression = GetQueryExpression(sandbox.FakeCrmDbConnection, sql);
+                // Assert
+                Assert.That(queryExpression.ColumnSet.AllColumns == false);
+                Assert.That(queryExpression.ColumnSet.Columns[0] == "contactid");
+                Assert.That(queryExpression.ColumnSet.Columns[1] == "firstname");
+                Assert.That(queryExpression.ColumnSet.Columns[2] == "lastname");
+                Assert.That(queryExpression.EntityName == "contact");
+            }
+        }
+
     }
 }
